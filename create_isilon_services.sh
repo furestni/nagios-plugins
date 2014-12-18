a982dfa9e945f7c3c1b04040416be78cc81922ad
Martin Kolly <martin.kolly@swisstxt.ch>
2014-12-18 07:49:02 +0100

    fix service.vars definition

--- create_isilon_services.sh
+++ create_isilon_services.sh
@@ -9,53 +9,50 @@ excludefilter=''
 
 for hostname in Isilon-ix-SmartConnect.stxt.media.int Isilon-cu01-SmartConnect.stxt.media.int
 do
-
-if [ $hostname == "Isilon-ix-SmartConnect.stxt.media.int" ]
-then
-  servicelist=$( ssh root@isilon-ix-smartconnect.stxt.media.int -C "isi quota ls" | awk '$5 ~ "[[:digit:]]" { print $3 }'  )
-  cfgfile="/etc/icinga2/zones.d/datacenter-ix/isilonshare.cfg"
-else
-  servicelist=$( ssh root@isilon-cu01-smartconnect.stxt.media.int -C "isi quota ls" | awk '$5 ~ "[[:digit:]]" { print $3 }' )
-  cfgfile="/etc/icinga2/zones.d/datacenter-bie/isilonshare.cfg"
-fi
-
-
-if [[ ! -z $servicelist ]]
-then
-   # Write host definition for Smart Connect
-  echo -e "object Host \"$hostname\" { 
-  import \"generic-host\"
-  address = \"$hostname\"
-  } " >  $cfgfile.tmp
-  
-  # Write Services for Smart Connect Host
-  for y in $servicelist
-  do 
-     echo -e "apply service \"Isilon_share $y\"{
-     import \"generic-service-pnp\"
-     vars.sla = \"24x7\"
-
-     check_command  = \"check_by_ssh\"
-     
-     vars.user = \"root\" 
-     vars.timeout = \"30\"
-     vars.command = \"/bin/bash /ifs/data/nagios/isilon-quota-usage.sh -p $y -w $warninglevel -c $criticallevel\"
-      } \n " >> $cfgfile.tmp
-   done
-   
-   # Check if modification
-   diff -r cfgfile cfgfile.tmp > /dev/null 2>&1
-   if [ $? -eq 1 ]
-     then
-     echo "Config Changed, updating now"
-     cp $cfg.tmp $cfg
-     echo "new or modified isilon shares, reload icinga"
-     cp -rp /var/icinga/core/dynamic_hosts /etc/icinga/
+  if [ $hostname == "Isilon-ix-SmartConnect.stxt.media.int" ]
+  then
+    servicelist=$( ssh root@isilon-ix-smartconnect.stxt.media.int -C "isi quota ls" | awk '$5 ~ "[[:digit:]]" { print $3 }'  )
+    cfgfile="/etc/icinga2/zones.d/datacenter-ix/isilonshare.cfg"
   else
-      echo "no isilon shares modifications, nothing to do"
+    servicelist=$( ssh root@isilon-cu01-smartconnect.stxt.media.int -C "isi quota ls" | awk '$5 ~ "[[:digit:]]" { print $3 }' )
+    cfgfile="/etc/icinga2/zones.d/datacenter-bie/isilonshare.cfg"
   fi
-  
+
+  if [[ ! -z $servicelist ]]
+  then
+     # Write host definition for Smart Connect
+    echo -e "object Host \"$hostname\" { 
+    import \"generic-host\"
+    address = \"$hostname\"
+    } " >  $cfgfile.tmp
+    
+    # Write Services for Smart Connect Host
+    for y in $servicelist
+    do 
+       echo -e "apply service \"Isilon_share $y\"{
+       import \"generic-service-pnp\"
+       vars.sla = \"24x7\"
+
+       check_command  = \"check_by_ssh\"
+       
+       vars.user = \"root\" 
+       vars.timeout = \"30\"
+       vars.option = \"StrictHostKeyChecking=no\"
+       vars.command = \"/bin/bash /ifs/data/nagios/isilon-quota-usage.sh -p $y -w $warninglevel -c $criticallevel\"
+        } \n " >> $cfgfile.tmp
+    done
+     
+     # Check if modification
+    diff cfgfile cfgfile.tmp > /dev/null 2>&1
+    if [ $? -eq 1 ]
+    then
+      echo "Config Changed, updating now"
+      cp $cfgfile.tmp $cfgfile
+    else
+      echo "no isilon shares modifications, nothing to do"
+    fi
+  fi
 done
-fi
+
 
 #chown icinga.icinga $isiloncfgpath
