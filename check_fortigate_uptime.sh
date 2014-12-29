a505a703030204738edab24daa267f5c1c464cf9
Martin Kolly <martin.kolly@swisstxt.ch>
2014-12-29 10:45:47 +0100

    revert check_fortigate_uptime.sh

--- check_fortigate_uptime.sh
+++ check_fortigate_uptime.sh
@@ -1,22 +1,58 @@
 #!/bin/bash
 
-#USAGE
-#./check_fortigate_uptime.pl {Host} {Community}
+oid=.1.3.6.1.2.1.1.3.0
 
-snmpget -v1 -c $2 $1 .1.3.6.1.2.1.1.3.0 | grep days > /dev/null
+# Argument Parsing
+function usage {
+   echo "usage:"
+   echo "-H Fortigate Host"
+   echo "-C Community"
+   exit 3
+}
+
+# Catch Arguments
+while getopts ":H:C:" optname
+do
+  case "$optname" in
+    "H")
+      host=$OPTARG
+      ;;
+    "C")
+      community=$OPTARG
+      ;;
+    "?")
+      echo "Unknown option $OPTARG"
+      usage
+      ;;
+    ":")
+      echo "No argument value for option $OPTARG"
+      usage
+      ;;
+    *)
+      #Should not occur
+      echo "Unknown error while processing options"
+      usage
+      ;;
+  esac
+done
+
+
+snmpget -v1 -c $community $host $oid | grep days > /dev/null
 RETVAL=$?
+
+output=$(snmpget -v1 -c $community $host $oid)
 
 if [ $RETVAL -eq 0 ]
   then
-  days=`snmpwalk -v 1 $1 -c $2 .1.3.6.1.2.1.1.3.0 | awk '{print $5}'`
-  hours=`snmpwalk -v 1 $1 -c $2 .1.3.6.1.2.1.1.3.0 | awk '{print $7}' | awk -F ':' '{print $1}' | sed 's/^0//'`
-  minutes=`snmpwalk -v 1 $1 -c $2 .1.3.6.1.2.1.1.3.0 | awk '{print $7}' | awk -F ':' '{print $2}' | sed 's/^0//'`
+  days=`echo $output | awk '{print $5}'`
+  hours=`echo $output | awk '{print $7}' | awk -F ':' '{print $1}' | sed 's/^0//'`
+  minutes=`echo $output | awk '{print $7}' | awk -F ':' '{print $2}' | sed 's/^0//'`
   echo "OK Uptime > 1 day:  $days day(s) $hours hour(s) and $minutes minute(s) | day(s)=$days"
   exit 0
 else
   days=0
-  hours=`snmpwalk -v 1 $1 -c $2 .1.3.6.1.2.1.1.3.0 | awk '{print $5}' | awk -F ':' '{print $1}' | sed 's/^0//'`
-  minutes=`snmpwalk -v 1 $1 -c $2 .1.3.6.1.2.1.1.3.0 | awk '{print $5}' | awk -F ':' '{print $2}' | sed 's/^0//'`
+  hours=`echo $output | awk '{print $5}' | awk -F ':' '{print $1}' | sed 's/^0//'`
+  minutes=`echo $output | awk '{print $5}' | awk -F ':' '{print $2}' | sed 's/^0//'`
   echo "Warning Uptime < 1 day: Uptime $days day(s) $hours hour(s) and $minutes minute(s) | day(s)=$days"
   exit 1
 fi
