42e1d44e8d7aa4621bc61500071850be18f62667
Daniel Menet <membership@sontags.ch>
2015-02-25 11:09:25 +0100

    improved links of jenkins view plugin

--- check_jenkins_view.py
+++ check_jenkins_view.py
@@ -69,7 +69,7 @@ def checkview(view, alarm):
     for job in view["jobs"]:
         for state in alarmstates:
             if job["color"] == state:
-                link = "<a href=\"{0}\">{1}</a>".format(job["url"], job["name"])
+                link = "<a href=\"{0}\" target=\"_blank\">{1}</a>".format(job["url"], job["name"])
                 failed.append(link)
 
     return failed
