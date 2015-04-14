538caa4f7a5204d1b1a0b883aca6bf1bbda79c8f
Simon Isenschmid <simon@ironsmith.ch>
2015-04-14 15:32:22 +0200

    fixed check_wowza_listener perl script

--- check_wow_listeners.pl
+++ check_wow_listeners.pl
@@ -3,11 +3,12 @@
 use strict;
 use XML::LibXML;
 use Getopt::Long;
+use LWP::UserAgent;
 use lib "/usr/lib/nagios/plugins";
 use lib "/usr/local/nagios/libexec";
 use utils qw($TIMEOUT %ERRORS &print_revision &support);
 
-my $staturl = "http://" . $ARGV[0] . ":8086/connectioncounts";
+# Global Definitions
 my $user = '';
 my $pass = '';
 my $hostname = '';
@@ -19,33 +20,25 @@ GetOptions(
         "u=s" => \$user
 ) or print_help();
 
+# Call Wowza API to get XML
 my $staturl = "http://" . $hostname . ":8086/connectioncounts";
-
-#get listeners
-use LWP::UserAgent;
 my $ua = LWP::UserAgent->new;
 my $req = HTTP::Request->new(GET => $staturl);
-$ua->credentials($ARGV[0].":8086","Wowza Media Systems",$user,$pass);
-
+$ua->credentials($hostname.":8086","Wowza Media Systems",$user,$pass);
 my $res = $ua->request($req);
 
+# Parse XML to get Current Listeners
 my $parser = XML::LibXML->new();
 my $tree = $parser->parse_string($res->content);
 my $root = $tree->getDocumentElement;
-
-
-
 my $query = "//WowzaMediaServer/VHost/Application[Name/text() = 'live']/ConnectionsCurrent/text()";
+
+# Get what you need and print
 $listeners = $_->data ."\n" foreach ($root->findnodes($query));
-
-
-
-
-#my $noutput = "total listeners = " . $totallisteners . " | total_listeners=" . $totallisteners . ", " . $perfdata;
-#$noutput =~ s/, $//g;
 my $noutput = "total listeners = " . $listeners . " | total_listeners=" . $listeners;
 print $noutput . "\n";;
 
+# Help
 sub print_help {
         printf "Plugin to monitor wowza listeners \n";
         printf "\nUsage:\n";
