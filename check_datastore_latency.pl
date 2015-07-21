fb3f9386b22f7f444bdae192081c369503a2709e
Michael Pospiezsynski <michael.pospiezsynski@swisstxt.ch>
2015-07-21 09:08:38 +0200

    disable ssl verification

--- check_datastore_latency.pl
+++ check_datastore_latency.pl
@@ -241,3 +241,8 @@ Vim::logout();
 
 print "EXIT CODE=",$exit_code,"\n";
 exit ($exit_code);
+
+BEGIN {
+   $ENV{PERL_NET_HTTPS_SSL_SOCKET_CLASS} = "Net::SSL";
+   $ENV{PERL_LWP_SSL_VERIFY_HOSTNAME} = 0;
+}
