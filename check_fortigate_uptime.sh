93ef5d89bcd1fedd5b876349565ac3cdde692fbd
Martin Kolly <martin.kolly@swisstxt.ch>
2015-07-31 10:35:11 +0200

    fix foritage uptime script

--- check_fortigate_uptime.sh
+++ check_fortigate_uptime.sh
@@ -37,7 +37,7 @@ do
 done
 
 
-snmpget -v1 -c $community $host $oid | grep days > /dev/null
+snmpget -v1 -c $community $host $oid | grep day > /dev/null
 RETVAL=$?
 
 output=$(snmpget -v1 -c $community $host $oid)
@@ -47,13 +47,13 @@ if [ $RETVAL -eq 0 ]
   days=`echo $output | awk '{print $5}'`
   hours=`echo $output | awk '{print $7}' | awk -F ':' '{print $1}' | sed 's/^0//'`
   minutes=`echo $output | awk '{print $7}' | awk -F ':' '{print $2}' | sed 's/^0//'`
-  echo "OK Uptime > 1 day:  $days day(s) $hours hour(s) and $minutes minute(s) | day(s)=$days"
+  echo "OK Uptime > 1 day:  $days day(s) $hours hour(s) and $minutes minute(s) | days=$days"
   exit 0
 else
   days=0
   hours=`echo $output | awk '{print $5}' | awk -F ':' '{print $1}' | sed 's/^0//'`
   minutes=`echo $output | awk '{print $5}' | awk -F ':' '{print $2}' | sed 's/^0//'`
-  echo "Warning Uptime < 1 day: Uptime $days day(s) $hours hour(s) and $minutes minute(s) | day(s)=$days"
+  echo "Warning Uptime < 1 day: Uptime $days day(s) $hours hour(s) and $minutes minute(s) | days=$days"
   exit 1
 fi
 
