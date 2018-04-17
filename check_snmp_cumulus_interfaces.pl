4298040ad41d5264a984fcfaa05136147e92f071
Michael Pospiezsynski <michael.pospiezsynski@swisstxt.ch>
2018-04-17 16:28:50 +0200

    add ignore option

--- check_snmp_cumulus_interfaces.pl
+++ check_snmp_cumulus_interfaces.pl
@@ -30,6 +30,7 @@ GetOptions (
     "man"         => \$opt{'man'},
 	"tofile=s"    => \$opt{'tofile'},
 	"fromfile=s"  => \$opt{'fromfile'},
+	"ignore=s"	  => \$opt{'ignore'},
 	"verbose"     => \$opt{'verbose'},
 ) or pos2usage(2);
 
@@ -128,16 +129,20 @@ foreach my $idLong (keys($response->{'ifAlias'})) {
     $idShort = $idLong;
     $idShort =~ s/^\.1\.3\.6\.1\.2\.1\.31\.1\.1\.1\.18\.//;
     $interface->{$idShort}{'ifAlias'} = $response->{'ifAlias'}{$idLong};
+	$interface->{$idShort}{'ifIgnore'} = defined($opt{'ignore'}) ? grep (/^$response->{'ifAlias'}{$idLong}$/, split (",", $opt{'ignore'})) : 0;
 }
+
+
+
 
 $rc = 0;
 foreach my $id (sort(keys($interface))) {
 #	printf "%3d	  %-32s   %-20s %s\n", $id, $interface->{$id}{'ifAlias'}, $txtAdminStatus[$interface->{$id}{'ifAdminStatus'}], $txtOperStatus[$interface->{$id}{'ifOperStatus'}];
 #	printf "%3d	  %-32s   %s\n", $id, $interface->{$id}{'ifAlias'}, $txtOperStatus[$interface->{$id}{'ifOperStatus'}] if ($interface->{$id}{'ifAdminStatus'} == 1);
 	if (($interface->{$id}{'ifAdminStatus'} == 1) && ($interface->{$id}{'ifOperStatus'} != 1)) {
-		printf "%3d	  %-32s   %s\n", $id, $interface->{$id}{'ifAlias'}, $txtOperStatus[$interface->{$id}{'ifOperStatus'}] if ($opt{'verbose'});
-		push(@msg, sprintf("%s=%s", $interface->{$id}{'ifAlias'}, $txtOperStatus[$interface->{$id}{'ifOperStatus'}]));
-		$rc=2;
+		printf "%3d	  %-32s   %s	%s\n", $id, $interface->{$id}{'ifAlias'}, $txtOperStatus[$interface->{$id}{'ifOperStatus'}], $interface->{$id}{'ifIgnore'} > 0 ? "(ignored)" : "" if ($opt{'verbose'});
+		push(@msg, sprintf("%s=%s%s", $interface->{$id}{'ifAlias'}, $txtOperStatus[$interface->{$id}{'ifOperStatus'}], $interface->{$id}{'ifIgnore'} > 0 ? "(ignored)" : ""));
+		$rc=2 if ($interface->{$id}{'ifIgnore'} == 0 );
 	}
 }
 
@@ -156,7 +161,7 @@ __END__
 
 =head1 NAME
 
-VPN Check
+Check Port States of Cumulus Switch
 
 =head1 SYNOPSIS
 
@@ -173,6 +178,10 @@ usage:
 =item B<--community string>
 
     Community String, default: public
+
+=item B<--ignore item{,item}>
+
+	port alias names to ignore the actual state for monitoring
 
 =item B<--tofile filename>
 
@@ -198,6 +207,6 @@ usage:
 
 =head1 DESCRIPTION
 
-check port status of Cumulus Switches
+Check Port State of Cumulus Switch. Print all Ports which are Admin UP and Operation State is not UP. If all Admin UP Ports are Operation State UP, just print OK.
 
 =cut
